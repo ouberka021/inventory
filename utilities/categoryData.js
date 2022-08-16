d1ee75a524decdef39355bf2cbbf8b527e8195fd
Taieb OUBERKA <obk@Taiebs-MBP.fios-router.home>
2022-08-16 14:24:52 -0400

    first app

--- utilities/categoryData.js
+++
@@ -1,8 +0,0 @@
-const categoryData = [
-    {
-        name: 'Bins',
-        img: 'http://www.clipartbest.com/cliparts/9cz/rMM/9czrMMBcE.jpeg'
-       
-      }
- ];
-module.exports = categoryData;
--- utilities/userData.js
+++
@@ -1,9 +0,0 @@
-const userData = [
-    {
-      firstname: "Taieb",
-      lastname: "OUBERKA",
-      email: "ouberka021@gmail.com",
-      password: "fatima79"
-      }
- ];
-module.exports = userData;
